902e3846be5c6c87cfec171a9ae513b7a72d72a0
gudnm <dfobox@gmail.com>
2017-06-01 14:07:42 -0400

    Change file IO in listAndRun to be asynchronous

--- list-and-run.js
+++ list-and-run.js
@@ -5,20 +5,34 @@ const fs = require('fs');
 const path = '/tmp/te';
 
 module.exports = function listAndRun() {
-    var text = [];
 
-    const listing = fs.readFileSync(path, 'utf8');
+    let readFileAsync = function(filename, encoding) {
+        return new Promise(function(resolve, reject) {
+            fs.readFile(filename, encoding, (data, err) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(data);
+            });
+        });
+    };
+    
+    readFileAsync(path, 'utf8')
+    .then(listing => {
+        var text = [];
 
-    if (listing.length > 0) {
-        text.push(addLineNumbers(listing) + '\n');
+        if (listing.length > 0) {
+            text.push(addLineNumbers(listing) + '\n');
+            const child = spawnSync('node', [path], {timeout: 100, maxBuffer: 100});
+            text.push(`stdout: ${child.output[1]}`);
+            text.push(`stderr: ${child.output[2]}`);
+        } else {
+            text.push("Code listing is empty\n");
+        }
 
-        const child = spawnSync('node', [path], {timeout: 100, maxBuffer: 100});
-        text.push(`stdout: ${child.output[1]}`);
-        text.push(`stderr: ${child.output[2]}`);
-
-    } else {
-        text.push("Code listing is empty\n");
-    }
-
-    console.log(text.join(''));
+        console.log(text.join(''));
+    })
+    .catch(err => {
+        throw err;
+    })
 };
